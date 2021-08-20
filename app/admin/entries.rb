bd351a223d5d4757393182e36248973072d21fd4
AmalieMargretheMogensen <amaliemargrethe11@outlook.dk>
2021-08-20 10:36:36 +0200

    Guide 13+14

---
+++ app/admin/entries.rb
@@ -0,0 +1,6 @@
+ActiveAdmin.register Entry do
+
+  
+  permit_params :title, :date, :contents
+
+end
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,8 +9,18 @@ class EntriesController < ApplicationController
         @new_entry =Entry.new
     end
     def create
-        entry_params = params["entry"]
+        entry_params = params["entry"].permit("title", "contents")
         entry = Entry.create(entry_params)
         redirect_to(entry_path(entry))
     end
+    def edit
+        @entry = Entry.find(params["id"])
+    end
+    def update
+        entry_params = params["entry"].permit("title", "contents")
+        entry = Entry.find(params["id"])
+        entry.update(entry_params)
+        redirect_to(entry_path(entry))
+      end
 end
+
